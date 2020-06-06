24c0816814c7a16a479b2d6b9cebd96f23f67e5e
Atanas Atanasov <scriptex.bg@gmail.com>
2020-06-06 23:45:36 +0300

    Convert to Common JS

---
+++ api/insights.js
@@ -0,0 +1,30 @@
+// @ts-nocheck
+
+const express = require('express');
+
+const { getClient } = require('../lib/helpers');
+const { getInsightsFromClient } = require('../lib/insights');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.listen(3000);
+
+app.post('/insights', async (req, res) => {
+	const { repository } = req.body;
+
+	if (!repository) {
+		return res.status(400).send({
+			message: 'Invalid or missing repository.'
+		});
+	}
+
+	try {
+		const insights = await getInsightsFromClient(getClient(process.env.TOKEN), [repository]);
+
+		return res.json(insights);
+	} catch (e) {
+		console.log(e);
+		return res.status(e.status).send({ message: e.message });
+	}
+});
